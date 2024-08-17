21671e6f4d9a1879a3c03a6a79fb8a2fc43d96c8
RodrigoVidalGuzmanDiaz <78930192+RodrigoVidalGuzmanDiaz@users.noreply.github.com>
2024-08-16 18:10:25 -0600

    Finalizacion de la tarea 2 de programacion

---
+++ src/vista/main.java
@@ -0,0 +1,12 @@
+
+package vista;
+
+public class main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_cadenas frm = new frm_cadenas();
+        frm.show();
+    }
+    
+}
